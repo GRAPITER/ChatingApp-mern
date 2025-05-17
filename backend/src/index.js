641b94061346962a754ad9761181d1a69f0fd9fb
Asjad <151639740+GRAPITER@users.noreply.github.com>
2025-05-17 15:10:30 +0500

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -27,10 +27,11 @@ app.use("/api/message", router);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
+  
+  app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
+ 
 }
 
 server.listen(PORT, () => {
